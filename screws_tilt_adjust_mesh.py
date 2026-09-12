a1cb409c21c1a2b221a0f5a1ed14cf9fa3857e4b
anderd84 <anderdm11@hotmail.com>
2026-09-12 18:44:01 -0400

    testing if this loads

--- screws_tilt_adjust_mesh.py
+++ screws_tilt_adjust_mesh.py
@@ -5,11 +5,18 @@ class ScrewsTiltAdjustMesh:
     bedMesh : BedMesh
     screwTiltAdjust : ScrewsTiltAdjust
 
-
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.gcode = self.printer.lookup_object('gcode')
+        self.gcode.register_command("SCREWS_TILT_ADJUST_MESH",
+                                    self.cmd_SCREWS_TILT_ADJUST_MESH,
+                                    desc=self.cmd_SCREWS_TILT_ADJUST_MESH_help)
 
-
+    cmd_SCREWS_TILT_ADJUST_MESH_help = "Tool to help adjust bed leveling " \
+                                     "screws by calculating the number " \
+                                     "of turns to level it."
     def cmd_SCREWS_TILT_ADJUST_MESH(self, gcmd):
         pass
 
+def load_config(config):
+    return ScrewsTiltAdjustMesh(config)
