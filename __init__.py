a7a92ac262840e5dd2fdbbfd0a7df6d740af0c4b
anderd84 <anderdm11@hotmail.com>
2026-09-13 13:10:38 -0400

    trying something

--- __init__.py
+++ __init__.py
@@ -6,9 +6,10 @@ class PluginManager:
         self.printer = config.get_printer()
         self.plugin_val = config.get("plugin_val")
         self.name = config.get_name()
-        logging.info("LOADING PLUGIN MANAGER")
-        logging.info("name : %s", self.name)
+        logging.info("LOADING PLUGIN MANAGER\n")
+        logging.info("name : %s\n" % (self.name,))
         self.plugin = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
+        logging.info("DONE LOADING MANAGER")
 
 def load_config_prefix(config):
     return PluginManager(config)
