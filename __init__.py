fb82ddf00eea3f6a447e9547978513dcb65237fb
anderd84 <anderdm11@hotmail.com>
2026-09-13 13:06:45 -0400

    log name

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ class PluginManager:
         self.plugin_val = config.get("plugin_val")
         self.name = config.get_name()
         logging.info("LOADING PLUGIN MANAGER")
+        logging.info("name : %s", self.name)
         self.plugin = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
 
 def load_config_prefix(config):
