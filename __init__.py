f94fc8a50d2bcc42858d84943e4b62e7c9ba4ef5
anderd84 <anderdm11@hotmail.com>
2026-09-12 21:47:12 -0400

    does this work

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
 from . import screws_tilt_adjust_mesh
 
+class PluginManager:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.STAM = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
+
 def load_config(config):
-    return object()
+    return PluginManager(config)
