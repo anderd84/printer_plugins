0cf0af6d6bdd44756a06da271b637f4cbe5f4b3f
anderd84 <anderdm11@hotmail.com>
2026-09-13 13:28:42 -0400

    fix package resolution

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ class PluginManager:
         if not os.path.exists(py_name):
             raise self.config_error("Unable to load module '%s' (doesn't exist)" % (py_name,))
         
-        mod = importlib.import_module(self.plugin_name, ".")
+        mod = importlib.import_module(self.plugin_name, package=__package__)
 
         init_func = 'load_config'
         init_func = getattr(mod, init_func, None)
