cb079a06db8853dd32baba918ff007888a80aeb2
anderd84 <anderdm11@hotmail.com>
2026-09-13 16:10:39 -0400

    fix print

--- screws_tilt_adjust_mesh.py
+++ screws_tilt_adjust_mesh.py
@@ -34,7 +34,7 @@ class ScrewsTiltAdjustMesh:
         self.gcode.run_script_from_command("BED_MESH_PROFILE LOAD=STAM_mesh")
 
         z_mesh: ZMesh = self.bed_mesh.get_mesh()
-        z_mesh.print_mesh()
+        z_mesh.print_mesh(logging.info)
 
 
 def load_config(config):
