028aeeab91fffd19afe707de684cedc9a4a2cc50
anderd84 <anderdm11@hotmail.com>
2026-09-13 13:06:03 -0400

    send over

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ class PluginManager:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.plugin_val = config.get("plugin_val")
-        
+        self.name = config.get_name()
         logging.info("LOADING PLUGIN MANAGER")
         self.plugin = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
 
