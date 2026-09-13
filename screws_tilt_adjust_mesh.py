25543fc937bc17b4becf4ad9a988c6945ff60001
anderd84 <anderdm11@hotmail.com>
2026-09-13 16:54:08 -0400

    lets see what this does

--- screws_tilt_adjust_mesh.py
+++ screws_tilt_adjust_mesh.py
@@ -23,6 +23,25 @@ class ScrewsTiltAdjustMesh:
                                     self.cmd_SCREWS_TILT_ADJUST_MESH,
                                     desc=self.cmd_SCREWS_TILT_ADJUST_MESH_help)
 
+        self.gcode.register_command("STAM__PROCESS_MESH",
+                                    self.cmd_STAM__PROCESS_MESH,
+                                    desc=self.cmd_STAM__PROCESS_MESH_help)
+
+    def get_z_mesh(self, profile_name: str) -> ZMesh:
+        self.gcode.run_script_from_command("BED_MESH_PROFILE LOAD={profile_name}")
+        z_mesh: ZMesh = self.bed_mesh.get_mesh()
+
+    def process_mesh(self, z_mesh: ZMesh) -> None:
+        z_mesh.print_mesh(self.gcode.respond_info)
+        meshed_mat = z_mesh.get_mesh_matrix()
+
+        for y_ndx, y_line in enumerate(meshed_mat):
+            logging.info(y_line)
+            y_coord = z_mesh.get_y_coordinate(y_ndx)
+            for x_ndx, z_coord in enumerate(y_line):
+                x_coord = z_mesh.get_x_coordinate(x_ndx)
+                logging.info(f"x: {x_coord}, y: {y_coord}, z: {z_coord}")
+
 # ==========================================================================================
     cmd_SCREWS_TILT_ADJUST_MESH_help = "Tool to help adjust bed leveling " \
                                        "screws by calculating the number " \
@@ -31,10 +50,16 @@ class ScrewsTiltAdjustMesh:
         self.gcode.respond_info("Running custom gcode")
 
         self.gcode.run_script_from_command("BED_MESH_CALIBRATE PROFILE=STAM_mesh")
-        self.gcode.run_script_from_command("BED_MESH_PROFILE LOAD=STAM_mesh")
+        z_mesh = self.get_z_mesh("STAM_mesh")
+        self.process_mesh(z_mesh)
 
-        z_mesh: ZMesh = self.bed_mesh.get_mesh()
-        z_mesh.print_mesh(self.gcode.respond_info)
+
+    cmd_STAM__PROCESS_MESH_help = "TODO"
+    def cmd_STAM__PROCESS_MESH(self, gcmd):
+        profile = gcmd.get('PROFILE', "STAM_mesh")
+        z_mesh = self.get_z_mesh(profile)
+        self.process_mesh(z_mesh)
+
 
 
 def load_config(config):
