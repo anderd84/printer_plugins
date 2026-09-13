b1ce23d3930532092402ea2d6264c3cc1435aed9
anderd84 <anderdm11@hotmail.com>
2026-09-13 01:27:48 -0400

    test

--- __init__.py
+++ __init__.py
@@ -6,8 +6,7 @@ class PluginManager:
         self.printer = config.get_printer()
         self.test = config.get("test_val")
         logging.info("LOADING PLUGIN MANAGER")
-        self.STAM = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
-        self.printer.add_object(self.STAM)
+        self.plugin = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
 
-def load_config(config):
+def load_config_prefix(config):
     return PluginManager(config)
--- screws_tilt_adjust_mesh.py
+++ screws_tilt_adjust_mesh.py
@@ -23,6 +23,3 @@ class ScrewsTiltAdjustMesh:
                                        "of turns to level it."
     def cmd_SCREWS_TILT_ADJUST_MESH(self, gcmd):
         self.gcode.respond("Running custom gcode")
-
-def load_config(config):
-    return ScrewsTiltAdjustMesh(config)
