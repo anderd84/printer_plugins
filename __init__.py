efdb0712e1b88404851b0252ca8bb3e4603dba7d
anderd84 <anderdm11@hotmail.com>
2026-09-12 22:54:42 -0400

    send

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ import logging
 class PluginManager:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.test = config.get("test2")
-        logging.info(f"test2: {self.test}")
+        self.test = config.get("test_val")
+        logging.info("LOADING PLUGIN MANAGER")
         # self.STAM = screws_tilt_adjust_mesh.ScrewsTiltAdjustMesh(config)
 
 def load_config(config):
